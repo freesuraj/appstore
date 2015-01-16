a5f71bf9ca51d6fbcc80676af5162336dc0061e2
Suraj Pathak <freesuraj@gmail.com>
2015-01-16 18:03:36 +0800

    Change the body parser
    Sails uses skipper by default, which doesnot work well if the input
    parameters (text and files) are not in a particular order. Changed the
    body parser to 'formidable' which seems formidable indeed.

--- api/controllers/AppController.js
+++ api/controllers/AppController.js
@@ -32,121 +32,110 @@ module.exports = {
 
 	'createbuild': function(req, res, next)
 	{
-		// We're using Multiparty to parse a form that has both parts and text fields
-	  var multiparty = require('multiparty');
-	  var form = new multiparty.Form();
+		var uploadDir = './assets/uploads/';
+		var formidable = require('formidable');
+	    var form = new formidable.IncomingForm({
+	        uploadDir: uploadDir,
+	        keepExtensions: true
+	    });
 
 		form.parse(req, function(err, fields, files){
-		if(err){
-			req.session.flash = {err: err}
-			res.redirect('/app/newbuild/'+fields.appid);
+		if(err || !files.url){
+			var uploadError = [{
+				error: 'You must upload ipa file'
+			}]
+			req.session.flash = {err: uploadError}
+			res.redirect('/app/newbuild/'+ fields.owner);
 			return ;
-		}
-		var buildObj = {
-			version: fields.version,
-			detail: fields.appdetail,
-			owner: fields.appid
 		}
 
-		if(!fields.appid){
-			req.session.flash = {err: [{name: 'appId missing', message: 'App ID associated with this build couldnot be found'}]}
-			res.redirect('/app/newbuild/'+fields.appid);
+		if(!fields.owner || !fields.version){
+			req.session.flash = {err: [{name: 'missingValues', message: 'App ID and version number associated with this build couldnot be found'}]}
+			res.redirect('/app/newbuild/'+fields.owner);
 			return ;
+		}
+
+		var url;
+		if(files.url.size) {
+			url =  files.url.path.replace(/[^\/][^\/.]*\//,'/');
+		}
+
+		var buildObj = {
+			version: fields.version,
+			detail: fields.detail,
+			owner: fields.owner,
+			url: url
 		}
 
 		Build.create(buildObj, function buildCreated(err, build) {
 			if (err) {
-				req.session.flash = {err: err}
-				res.redirect('/app/newbuild/'+fields.appid);
+				var uploadError = [{
+				error: 'Error in creating build. Please try again.'
+			}]
+				req.session.flash = {err: uploadError}
+				res.redirect('/app/newbuild/'+fields.owner);
 				return ;
 			}
-			// Upload the app build
-        	var Up = require("./FileController");
-			if(req.file('appbuild'))
-			{
-				console.log("i do have stuffs to upload.. \n trying to dump em over to s3");
-				res.setTimeout(60000);
-				Up.s3upload(req,'appbuild', function(err, filesJson)
-				{
-					if(!err)
-					{
-						build.url = filesJson[0].fd.extra.Location;
-						build.url =  "/uploads/"+require('path').basename(fullDir);
-						var plistDir =   require('path').dirname(fullDir)+"/test.plist";
-						writeplist.writePlist(plistDir,"build.app.bundleid",build.version, build.url,"build.app.name",function(err, data){
-							if(!err) build.plist = data;
-						});
-					}
-					build.save(function (err, build) {
-						res.redirect('app/showbuild/' + build.id);
-					});
-				});
-			}
-			else 
-			{
-				build.save(function (err, build) {
+			// save file path
+        	build.save(function (err, build) {
 				res.redirect('app/showbuild/' + build.id);
 				});
-			}
-		});
+			});
 
 		});
 	},
 
-	create: function(req, res, next) 
+create: function(req, res, next) 
+{
+	var uploadDir = './assets/uploads/';
+	var formidable = require('formidable');
+    var form = new formidable.IncomingForm({
+        uploadDir: uploadDir,
+        keepExtensions: true
+    });
+
+	form.parse(req, function(err, fields, files)
 	{
-		var appObj = {
-			name: req.param('appName'),
-			detail: req.param('appDetail'),
-			bundleid: req.param('appBundleId'),
-		};
-
-		if(!appObj.name || !appObj.detail || !appObj.bundleid){
+		if(err || !fields.name || !fields.bundleid || !fields.detail) {
 			var missingFieldsError = [{
-				name: 'missingFields',
+				name: 'MissingFields',
 				message: 'App Name, App bundle Id and App detail are required !'
-			}]
-
+			}]		
 			req.session.flash = {err: missingFieldsError}
+			res.redirect('/app/new/');
+			return ;
+		}
+
+		var picture;
+		if(files.picture.size) {
+			picture =  files.picture.path.replace(/[^\/][^\/.]*\//,'/');
+		}
+
+		var appObj = {
+			name: fields.name,
+			bundleid: fields.bundleid,
+			detail: fields.detail,
+			picture: picture
+		}
+		console.log("files: " + JSON.stringify(files));
+
+	// Create an App with the params sent from the form
+	App.create(appObj, function appCreated(err, app)
+	{
+		if (err) {
+			req.session.flash = {err: err}
 			res.redirect('/app/new');
 			return ;
 		}
 
-		// Create an App with the params sent from the form
-		App.create(appObj, function appCreated(err, app)
-		{
-			if (err) {
-				req.session.flash = {err: err}
-				res.redirect('/app/create');
-				return ;
-			}
-
-			if(req.file('picture'))
-			{
-				console.log("trying to upload picture to s3");
-				var Up = require("./FileController");
-				res.setTimeout(60000);
-				Up.s3upload(req,'picture', function(err, filesJson)
-				{
-					if(!err)
-					{
-						console.log("upload result "+ JSON.stringify(filesJson));
-						app.picture = filesJson[0].extra.Location;
-						// app.picture =  "/uploads/"+require('path').basename(fullDir);
-					}
-					app.save(function (err, app) {
-						res.redirect('app/show/' + app.id);
-					});
-				});
-			}
-			else 
-			{
-				app.save(function (err, app) {
-				res.redirect('app/show/' + app.id);
-				});
-			}
+		if(files.picture.path)
+			// app.picture = files.picture.path;
+			app.save(function (err, app) {
+			res.redirect('app/show/' + app.id);
+			});
 		});
-	},
+	});
+},
 
 	// render the profile view (e.g. /views/show.ejs)
   show: function(req, res, next) {
@@ -194,116 +183,115 @@ module.exports = {
 
   'updatebuild': function(req, res, next)
 	{
-		// We're using Multiparty to parse a form that has both parts and text fields
-	  var multiparty = require('multiparty');
-	  var form = new multiparty.Form();
+		var uploadDir = './assets/uploads/';
+		var formidable = require('formidable');
+	    var form = new formidable.IncomingForm({
+	        uploadDir: uploadDir,
+	        keepExtensions: true
+	    });
 
 		form.parse(req, function(err, fields, files){
-			var buildObj, buildId;
 		if(err){
-			buildObj = {
-			version: req.param('version'),
-			detail: req.param('appdetail'),
-			};
-			buildId = req.param('buildid');
+			var uploadError = [{
+				error: 'There was some error updating the build, try again later or create a new one.'
+			}]
+			req.session.flash = {err: uploadError}
+			res.redirect('/app/editbuild/'+ fields.id);
+			return ;
 		}
-		else {
-			buildObj = {
+
+		if(!fields.id || !fields.version){
+			req.session.flash = {err: [{name: 'missingValues', message: 'App ID and version number associated with this build couldnot be found'}]}
+			res.redirect('/app/editbuild/'+fields.id);
+			return ;
+		}
+
+		var buildObj = {
 			version: fields.version,
-			detail: fields.appdetail,
-			}
-			buildId = fields.buildid;
-		} 
-
-		if(!buildId){
-			return res.badRequest();
+			detail: fields.detail,
 		}
 
-		Build.update(fields.buildid, buildObj, function buildCreated(err, build) {
+		if(files.url.size) {
+			var buildObj = {
+			version: fields.version,
+			detail: fields.detail,
+			url: files.url.path.replace(/[^\/][^\/.]*\//,'/')
+			}
+		}
+
+		Build.update(fields.id, buildObj, function buildUpdated(err, build) {
 			if (err) {
-				console.log("the error" + err);
-				return res.send({error: "error"});
+				var uploadError = [{
+				error: 'Error in updating build. Please try again.'
+			}]
+				req.session.flash = {err: uploadError}
+				res.redirect('/app/editbuild/'+fields.id);
+				return ;
 			}
-			// Upload the app build
-        	var Up = require("./FileController");
-			if(req.files)
-			{
-				res.setTimeout(6000);
-				Up.upload(req,'appbuild', function(err, filesJson)
-				{
-					console.log("build upload result "+ err || filesJson);
-					if(!err)
-					{
-						fullDir = filesJson[0].fd;
-						build[0].url =  "/uploads/"+require('path').basename(fullDir);
-						var plistDir =   require('path').dirname(fullDir)+"/test.plist";
-						writeplist.writePlist(plistDir,"build.app.bundleid",build.version, build.url,"build.app.name",function(err, data){
-							if(!err) build[0].plist = data;
-						});
-					}
-					build[0].save(function (err, build) {
-						res.redirect('app/showbuild/' + build.id);
-					});
-				});
-			}
-			else 
-			{
-				build[0].save(function (err, build) {
+			// save file path
+        	build[0].save(function (err, build) {
 				res.redirect('app/showbuild/' + build.id);
 				});
-			}
-		});
+			});
 
 		});
-	},
+},
 
   // process the info from edit view
-  update: function(req, res, next) {
+update: function(req, res, next) {
 
-      var appObj = {
-        name: req.param('appName'),
-        detail: req.param('appDetail'),
-        bundleid: req.param('appBundleId')
-      }
-
-    console.log("update request..");
-
-    App.update(req.param('appid'), appObj, function appUpdated(err, app) {
-      if (err) {
-      	console.log("error in update "+ err);
-        return res.redirect('/app/edit/' + req.param('appid'));
-      }
-      else {
-      	// Upload the app icon if it's changed
-      	if(req.files)
-      	{
-			console.log("upload coming.." + req.file('picture'));
-        	var Up = require("./FileController");
-			if(req.file('picture'))
-			{
-				res.setTimeout(6000);
-				Up.upload(req,'picture', function(err, filesJson)
-				{
-					if(!err)
-					{
-						fullDir = filesJson[0].fd;
-						app[0].picture =  "/uploads/"+require('path').basename(fullDir);
-					}
-					app[0].save(function (err, app) {
-						res.redirect('app/show/' + app.id);
-					});
-				});
-			}
-			else 
-			{
-				app[0].save(function (err, app) {
-				res.redirect('app/show/' + app.id);
-				});
-			}
-		}
-      }
-      
+    var uploadDir = './assets/uploads/';
+	var formidable = require('formidable');
+    var form = new formidable.IncomingForm({
+        uploadDir: uploadDir,
+        keepExtensions: true
     });
+
+	form.parse(req, function(err, fields, files)
+	{
+		if(err) {
+			var missingFieldsError = [{
+				name: 'Server Error',
+				message: 'Server Error, try again later'
+			}]		
+			req.session.flash = {err: missingFieldsError}
+			res.redirect('/app/new/');
+			return ;
+		}
+
+		var appObj = {
+			name: fields.name,
+			bundleid: fields.bundleid,
+			detail: fields.detail,
+		}
+
+		console.log("new file " + JSON.stringify(files));
+
+		if(files.picture.size) {
+				appObj = {
+					name: fields.name,
+					bundleid: fields.bundleid,
+					detail: fields.detail,
+					picture: files.picture.path.replace(/[^\/][^\/.]*\//,'/')
+				}
+		}
+
+	// Create an App with the params sent from the form
+	App.update(fields.id,appObj, function appCreated(err, apps)
+	{
+		if (err) {
+			req.session.flash = {err: err}
+			res.redirect('/app/edit');
+			return ;
+		}
+
+		if(files.picture.path)
+			// app.picture = files.picture.path;
+			apps[0].save(function (err, app) {
+			res.redirect('app/show/' + fields.id);
+			});
+		});
+	});
   },
 
   destroy: function(req, res, next) {
